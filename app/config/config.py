1cdef0f29b2c88902bdbb99fdb0467093f63133f
Juander <Juandercn@gmail.com>
2021-08-16 10:40:13 -0400

    Harversine v1.1

--- app/config/config.py
+++ app/config/config.py
@@ -4,14 +4,14 @@ class development():
     DEBUG = True
     HOST = "localhost"
     PORT = os.environ.get("PORT",7575) #It take a default port, if not take 7575
-    SECRET_KEY = 'JAJKFK325%432@!'
+    SECRET_KEY = 'JAJKFK325%432@'
 
 class production():
     class_name = "production_config"
     FLASK_ENV = 'production'
     HOST = "0.0.0.0"
     PORT = 7575
-    SECRET_KEY = 'JAJKFK325%432@!'
+    SECRET_KEY = 'JAJKFK325%432@'
 
 config = {
     "development":development,
